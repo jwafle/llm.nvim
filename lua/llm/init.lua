076ccd838a9ede18ebe2bdb780e3ae91205a9e0a
Jared Woelfel <jared.woelfel@outlook.com>
2024-09-26 23:00:04 -0500

    expose lua.api funcs

--- lua/llm/init.lua
+++ lua/llm/init.lua
@@ -1,8 +1,8 @@
 local M = {}
 local api = require("llm.api")
 
-function M.create_llm(opts, make_curl_args_fn, handle_data_fn)
-	api.invoke_llm_and_stream_into_editor(opts, make_curl_args_fn, handle_data_fn)
+for k, v in pairs(api) do
+	M[k] = v
 end
 
 return M
